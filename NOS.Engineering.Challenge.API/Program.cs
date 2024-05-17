9a588f737c68eb6fb2ac7c20fa87dc7662988c5e
Erick <erickn.hora@gmail.com>
2024-05-17 16:30:17 +0100

    Task 3 (Fix): - Added DI for custom services;

--- NOS.Engineering.Challenge.API/Program.cs
+++ NOS.Engineering.Challenge.API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using NOS.Engineering.Challenge.API.Extensions;
 using NOS.Engineering.Challenge.Database;
+using NOS.Engineering.Challenge.Managers;
+using NOS.Engineering.Challenge.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +15,16 @@ builder.Logging.AddFilter("Microsoft", LogLevel.Warning)
                .AddFilter("System", LogLevel.Warning)
                .AddFilter("Default", LogLevel.Information);
 
-// Register services
-
 // Add EF Core and SQLite
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=content.db"));
 
+// Add DI for custom services
+builder.Services.AddScoped<IMapper<Content, ContentDto>, ContentMapper>();
+builder.Services.AddScoped<IDatabase<Content, ContentDto>, DatabaseService>();
+builder.Services.AddScoped<IContentsManager, ContentsManager>();
+
+// Register services
 builder.ConfigureWebHost()
        .RegisterServices();
 
