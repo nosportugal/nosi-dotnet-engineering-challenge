d7be96181548ce2b41dc3130bd25d2edd7a4e946
Frederico <fredericotopa@gmail.com>
2024-05-17 14:04:36 +0100

    Revert "Tasks 1,2 and 6"
    This reverts commit 8ce7564c0eda40f1871eb820c151addd6e21a522.

--- NOS.Engineering.Challenge.API/Controllers/ContentController.cs
+++ NOS.Engineering.Challenge.API/Controllers/ContentController.cs
@@ -1,9 +1,7 @@
 using System.Net;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using NOS.Engineering.Challenge.API.Models;
 using NOS.Engineering.Challenge.Managers;
-using NOS.Engineering.Challenge.Models;
 
 namespace NOS.Engineering.Challenge.API.Controllers;
 
@@ -12,12 +10,9 @@ namespace NOS.Engineering.Challenge.API.Controllers;
 public class ContentController : Controller
 {
     private readonly IContentsManager _manager;
-    private readonly ILogger<ContentController> _logger;
-
-    public ContentController(IContentsManager manager, ILogger<ContentController> logger)
+    public ContentController(IContentsManager manager)
     {
         _manager = manager;
-        _logger = logger;
     }
     
     [HttpGet]
@@ -73,136 +68,20 @@ public class ContentController : Controller
     }
     
     [HttpPost("{id}/genre")]
-    public async Task<IActionResult> AddGenres(
+    public Task<IActionResult> AddGenres(
         Guid id,
         [FromBody] IEnumerable<string> genre
     )
     {
-        try
-        {
-            // Checks if id exists
-            var content = await _manager.GetContent(id);
-
-            if (content == null)
-            {
-                return NotFound();
-            }
-
-            // Checks if provided genres are actually new
-            var existingGenres = content.GenreList ?? Enumerable.Empty<string>();    
-            var uniqueGenres = genre
-                    .Where( g => !existingGenres.Contains(g, StringComparer.OrdinalIgnoreCase))
-                    .Distinct(StringComparer.OrdinalIgnoreCase)
-                    .ToList();
-        
-            if (uniqueGenres.Count == 0)
-            {
-                return BadRequest("The provided genres are not new");
-            }
-        
-            // Updates the content
-            var updatedContent = await _manager.UpdateContent(id, new ContentDto
-            (
-                title: null,
-                subTitle: null,
-                description: null,
-                imageUrl: null,
-                duration: null,
-                startTime: null,
-                endTime: null,
-                genreList: uniqueGenres
-            )
-            );
-
-            _logger. LogInformation("Genres added succcessfully to {Id}", id);
-
-            return Ok(updatedContent);
-        }
-        catch (Exception ex)
-        {
-            _logger. LogError(ex, "Error adding genres to {Id}", id);
-
-            return StatusCode(500, "Error occured while adding genres");
-        }
+        return Task.FromResult<IActionResult>(StatusCode((int)HttpStatusCode.NotImplemented));
     }
     
     [HttpDelete("{id}/genre")]
-    public async Task<IActionResult> RemoveGenres(
+    public Task<IActionResult> RemoveGenres(
         Guid id,
         [FromBody] IEnumerable<string> genre
     )
     {
-        try
-        {
-            // Checks if id exists
-            var content = await _manager.GetContent(id);
-
-            if (content == null)
-            {
-                return NotFound();
-            }
-
-            // Updates the content
-            var updatedGenres = content.GenreList.Except(genre, StringComparer.OrdinalIgnoreCase).ToList();
-                
-            if(updatedGenres.Count == content.GenreList.Count())
-            {
-                return BadRequest ("Provided genres not found");
-            }
-            var updatedContent = await _manager.UpdateContent(id, new ContentDto
-            (
-                title: null,
-                subTitle: null,
-                description: null,
-                imageUrl: null,
-                duration: null,
-                startTime: null,
-                endTime: null,
-                genreList: updatedGenres
-            )
-            );
-
-            _logger. LogInformation("Genres removed successfully from {Id}", id);
-
-            return Ok(updatedContent);
-        }
-
-        catch (Exception ex)
-        {
-            _logger. LogError(ex, "Error removing genres from {Id}", id);
-
-            return StatusCode(500, "Error occurred while removing genres");
-        }
-
-    }
-
-
-    [HttpGet("search")]
-    public async Task<IActionResult> SearchContents([FromQuery] string? title, [FromQuery] string? genre)
-    {
-        try
-        {
-            var contents = await _manager.GetManyContents();
-
-            if (!string.IsNullOrEmpty(title))
-            {
-                contents = contents.Where(c => c.Title.Contains(title, StringComparison.OrdinalIgnoreCase));
-                
-            }
-
-            if(!string.IsNullOrEmpty(genre))
-            {
-                contents = contents.Where(c => c.GenreList.Contains(genre, StringComparer.OrdinalIgnoreCase));
-            }
-
-            return Ok(contents);
-        }
-
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Error searching");
-
-            return StatusCode(500, "Error occurred while searching");
-        }
+        return Task.FromResult<IActionResult>(StatusCode((int)HttpStatusCode.NotImplemented));
     }
 }
