334d4e515c90cc559f645dd1a0b41f9741a5d7d8
TomasMirandaDevoteam <tomas.miranda@devoteam.com>
2024-06-28 08:52:07 +0000

    feat(challenge): Tasks #1, #2, #6

--- NOS.Engineering.Challenge/Managers/ContentsManager.cs
+++ NOS.Engineering.Challenge/Managers/ContentsManager.cs
@@ -1,25 +1,38 @@
+using Microsoft.Extensions.Logging;
 using NOS.Engineering.Challenge.Database;
 using NOS.Engineering.Challenge.Models;
-
 namespace NOS.Engineering.Challenge.Managers;
 
 public class ContentsManager : IContentsManager
 {
     private readonly IDatabase<Content?, ContentDto> _database;
+    private readonly ILogger<ContentsManager> _logger;
 
-    public ContentsManager(IDatabase<Content?, ContentDto> database)
+    public ContentsManager(IDatabase<Content?, ContentDto> database, ILogger<ContentsManager> logger)
     {
         _database = database;
+        _logger = logger;
     }
 
     public Task<IEnumerable<Content?>> GetManyContents()
     {
-        return _database.ReadAll();
+        var result = _database.ReadAll();
+        _logger.LogInformation($"{nameof(GetManyContents)} returned {result.Result.Count()} results");
+        return result;
     }
 
     public Task<Content?> CreateContent(ContentDto content)
     {
-        return _database.Create(content);
+        try
+        {
+            var result = _database.Create(content);
+            _logger.LogInformation($"Sucessfully created content with ID: {result.Result.Id}");
+            return result;
+        }
+        catch (Exception ex) {
+            _logger.LogError($"{ex}");
+            return null;
+        }
     }
 
     public Task<Content?> GetContent(Guid id)
@@ -29,11 +42,16 @@ public class ContentsManager : IContentsManager
 
     public Task<Content?> UpdateContent(Guid id, ContentDto content)
     {
-        return _database.Update(id, content);
+
+        var result = _database.Update(id, content);
+        _logger.LogInformation($"Sucessfully updated content with ID: {result.Result.Id}");
+        return result;
     }
 
     public Task<Guid> DeleteContent(Guid id)
     {
-        return _database.Delete(id);
+        var result = _database.Delete(id);
+        _logger.LogInformation($"Sucessfully deleted {result} with ID: {result.Result}");
+        return result;
     }
 }
